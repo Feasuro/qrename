dd6c2c2968ba7e030ca3caa312100afc1e8c0583
Feasuro <feasuro@protonmail.com>
2024-09-07 13:00:20 +0200

    advanced tab and renamers module

--- qrename.py
+++ qrename.py
@@ -4,141 +4,12 @@ import sys, os
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QDockWidget, QVBoxLayout,
-                             QGridLayout, QFormLayout, QListWidget, QFileDialog, QPushButton,
-                             QLineEdit, QComboBox, QSpinBox, QDateEdit, QLabel, QGroupBox,
-                             QMessageBox)
+                             QListWidget, QFileDialog, QPushButton, QMessageBox, QTabWidget)
 
-
-class Renamer(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setup_controls()
-        self.setup_layout()
-
-    def setup_layout(self):
-        """ Sets up the layout for the renamer window. """
-        # Basic controls
-        layout = QGridLayout()
-        layout.addWidget(QLabel('Prefix:'), 0, 0)
-        layout.addWidget(self.ptype, 0, 1)
-        layout.addWidget(self.pvalue, 0, 2)
-        layout.addWidget(QLabel('Suffix:'), 1, 0)
-        layout.addWidget(self.stype, 1, 1)
-        layout.addWidget(self.svalue, 1, 2)
-        layout.addWidget(QLabel('Name:'), 2, 0)
-        layout.addWidget(self.ntype, 2, 1)
-        layout.addWidget(self.nvalue, 2, 2)
-        layout.addWidget(QLabel('Extension:'), 3, 0)
-        layout.addWidget(self.etype, 3, 1)
-        layout.addWidget(self.evalue, 3, 2)
-        # Number
-        group = QGroupBox('Number')
-        grouplayout = QFormLayout()
-        grouplayout.addRow('Start number:', self.start)
-        grouplayout.addRow('Number of digits:', self.digits)
-        group.setLayout(grouplayout)
-        layout.addWidget(group, 4, 0, 1, 3)
-        # Date & Time
-        group = QGroupBox('Date and Time')
-        grouplayout = QFormLayout()
-        grouplayout.addRow('Date:', self.date)
-        group.setLayout(grouplayout)
-        layout.addWidget(group, 5, 0, 1, 3)
-        # set layout
-        self.setLayout(layout)
-
-    def setup_controls(self):
-        """ Sets up the transformation widgets for the selected files. """
-        # Prefix
-        self.pvalue = QLineEdit(self)
-        self.ptype = QComboBox(self)
-        self.ptype.addItems(['Custom', 'Number', 'Date'])
-        self.ptype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.pvalue))
-        # Suffix
-        self.svalue = QLineEdit(self)
-        self.stype = QComboBox(self)
-        self.stype.addItems(['Custom', 'Number', 'Date'])
-        self.stype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.svalue))
-        # Name
-        self.nvalue = QLineEdit(self)
-        self.nvalue.setDisabled(True)
-        self.ntype = QComboBox(self)
-        self.ntype.addItems(['Source name', 'lower case', 'UPPER CASE', 'Title Case',
-                             'Capitalize text', 'Custom name'])
-        self.ntype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.nvalue))
-        # Extension
-        self.evalue = QLineEdit(self)
-        self.evalue.setDisabled(True)
-        self.etype = QComboBox(self)
-        self.etype.addItems(['Source extension', 'lower case', 'UPPER CASE', 'Title Case',
-                             'Capitalize text', 'Custom extension'])
-        self.etype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.evalue))
-        # Number
-        self.digits = QSpinBox(self)
-        self.start = QSpinBox(self)
-        # Date
-        self.date = QDateEdit(self)
-        self.date.setDisplayFormat('dd-MM-yyyy')
-        self.date.setCalendarPopup(True)
-
-    def transform(self, string: str, index: int) -> str:
-        """ Performs the string transformation based on the selected options. """
-        result = ''
-        match self.ptype.currentText():
-            case 'Number':
-                result += str(self.start.value() + index).zfill(self.digits.value())
-            case 'Date':
-                result += self.date.date().toString('dd-MM-yyyy')
-            case 'Custom':
-                result += self.pvalue.text()
-        basename = os.path.splitext(os.path.basename(string))[0]
-        match self.ntype.currentText():
-            case 'Source name':
-                result += basename
-            case 'lower case':
-                result += basename.lower()
-            case 'UPPER CASE':
-                result += basename.upper()
-            case 'Title Case':
-                result += basename.title()
-            case 'Capitalize text':
-                result += basename.capitalize()
-            case 'Custom name':
-                result += self.nvalue.text()
-        match self.stype.currentText():
-            case 'Number':
-                result += str(self.start.value() + index).zfill(self.digits.value())
-            case 'Date':
-                result += self.date.date().toString('dd-MM-yyyy')
-            case 'Custom':
-                result += self.svalue.text()
-        extension = os.path.splitext(os.path.basename(string))[1]
-        match self.etype.currentText():
-            case 'Source extension':
-                result += extension
-            case 'lower case':
-                result += extension.lower()
-            case 'UPPER CASE':
-                result += extension.upper()
-            case 'Title Case':
-                result += extension.title()
-            case 'Capitalize':
-                result += extension.capitalize()
-            case 'Custom extension':
-                if self.evalue.text():
-                    result += '.'
-                    result += self.evalue.text()
-        return result
-
-    def deactivate_field(self, text: str, field: QLineEdit):
-        """ Deactivates the transformation fields when specific option is chosen """
-        if text.startswith('Custom'):
-            field.setDisabled(False)
-        else:
-            field.setDisabled(True)
-
+from renamers import BasicRenamer, AdvancedRenamer
 
 class RenameWindow(QMainWindow):
+    """ The main window for renaming files program. """
     def __init__(self):
         """ Initializes the main window and its components. """
         super().__init__()
@@ -148,18 +19,25 @@ class RenameWindow(QMainWindow):
         self.setup_ui()
         self.setup_signals()
 
-    def setup_ui(self):
+    def setup_ui(self) -> None:
         """ Creates the main window layout and widgets. """
         #create renamer central widget
-        self.renamer = Renamer()
-        self.setCentralWidget(self.renamer)
+        self.b_renamer = BasicRenamer()
+        self.a_renamer = AdvancedRenamer()
+        self.renamer = self.b_renamer
+        tabs = QTabWidget()
+        tabs.addTab(self.b_renamer, "Basic Renamer")
+        tabs.addTab(self.a_renamer, "Advanced Renamer")
+        tabs.currentChanged.connect(self.set_renamer)
+        self.setCentralWidget(tabs)
         #left dock for files management
         self.old_names = QListWidget()
-        self.open_button = QPushButton("Open Files")
+        open_button = QPushButton("Open Files")
+        open_button.clicked.connect(self.open_files)
         container = QWidget()
         layout = QVBoxLayout()
         layout.addWidget(self.old_names)
-        layout.addWidget(self.open_button)
+        layout.addWidget(open_button)
         container.setLayout(layout)
         dock = QDockWidget('Selected Files')
         dock.setFeatures(QDockWidget.DockWidgetFeature.NoDockWidgetFeatures)
@@ -167,48 +45,54 @@ class RenameWindow(QMainWindow):
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
         #right dock for new name presentation
         self.new_names = QListWidget()
-        self.rename_button = QPushButton("Rename Files")
+        rename_button = QPushButton("Rename Files")
+        rename_button.clicked.connect(self.rename_files)
         container = QWidget()
         layout = QVBoxLayout()
         layout.addWidget(self.new_names)
-        layout.addWidget(self.rename_button)
+        layout.addWidget(rename_button)
         container.setLayout(layout)
         dock = QDockWidget('New Names')
         dock.setFeatures(QDockWidget.DockWidgetFeature.NoDockWidgetFeatures)
         dock.setWidget(container)
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
 
-    def setup_signals(self):
-        """ Connects signals from the renamer and buttons to the main window. """
-        self.renamer.ptype.textActivated.connect(self.compute_names)
-        self.renamer.pvalue.textEdited.connect(self.compute_names)
-        self.renamer.stype.textActivated.connect(self.compute_names)
-        self.renamer.svalue.textEdited.connect(self.compute_names)
-        self.renamer.ntype.textActivated.connect(self.compute_names)
-        self.renamer.nvalue.textEdited.connect(self.compute_names)
-        self.renamer.etype.textActivated.connect(self.compute_names)
-        self.renamer.evalue.textEdited.connect(self.compute_names)
-        self.renamer.digits.valueChanged.connect(self.compute_names)
-        self.renamer.start.valueChanged.connect(self.compute_names)
-        self.renamer.date.dateChanged.connect(self.compute_names)
-        self.open_button.clicked.connect(self.open_files)
-        self.rename_button.clicked.connect(self.rename_files)
+    def setup_signals(self) -> None:
+        """ Connects signals from the renamers to the main window. """
+        self.b_renamer.ptype.textActivated.connect(self.compute_names)
+        self.b_renamer.pvalue.textEdited.connect(self.compute_names)
+        self.b_renamer.stype.textActivated.connect(self.compute_names)
+        self.b_renamer.svalue.textEdited.connect(self.compute_names)
+        self.b_renamer.ntype.textActivated.connect(self.compute_names)
+        self.b_renamer.nvalue.textEdited.connect(self.compute_names)
+        self.b_renamer.etype.textActivated.connect(self.compute_names)
+        self.b_renamer.evalue.textEdited.connect(self.compute_names)
+        self.b_renamer.digits.valueChanged.connect(self.compute_names)
+        self.b_renamer.start.valueChanged.connect(self.compute_names)
+        self.b_renamer.date.dateChanged.connect(self.compute_names)
 
-    def open_files(self):
+    def set_renamer(self, index: int) -> None:
+        """ Sets the current renamer based on the selected tab. """
+        if index == 0:
+            self.renamer = self.b_renamer
+        else:
+            self.renamer = self.a_renamer
+
+    def open_files(self) -> None:
         """ Opens file dialog and adds selected files to the docks. """
         self.files, _ = QFileDialog.getOpenFileNames(self, "Select Files", "", "All Files (*)")
         self.old_names.clear()
         self.new_names.clear()
         for index, file in enumerate(self.files):
             self.old_names.addItem(os.path.basename(file))
-            self.new_names.addItem(os.path.basename(self.renamer.transform(file, index)))
+            self.new_names.addItem(self.renamer.transform(file, index))
 
-    def compute_names(self):
+    def compute_names(self) -> None:
         """ Computes the new names for all files. """
         for index, file in enumerate(self.files):
             self.new_names.item(index).setText(self.renamer.transform(file, index))
 
-    def rename_files(self):
+    def rename_files(self) -> None:
         """ Renames files to the new names shown in the right dock. """
         answer = None
         new_files = list(self.files)
@@ -246,7 +130,7 @@ class RenameWindow(QMainWindow):
         self.new_names.clear()
         for index, file in enumerate(self.files):
             self.old_names.addItem(os.path.basename(file))
-            self.new_names.addItem(os.path.basename(self.renamer.transform(file, index)))
+            self.new_names.addItem(self.renamer.transform(file, index))
 
 
 if __name__ == "__main__":
---
+++ renamers.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+from abc import ABC, abstractmethod
+from PyQt6.QtWidgets import (QWidget, QGridLayout, QFormLayout, QGroupBox, QLabel, QLineEdit,
+                             QComboBox, QSpinBox, QDateEdit)
+
+class MetaRenamer(type(QWidget), type(ABC)):
+    """ Metaclass for creating renamer widgets. """
+    pass
+
+class Renamer(QWidget, ABC, metaclass=MetaRenamer):
+    """ Abstract base class for renaming widgets. """
+    @abstractmethod
+    def transform(self, path: str, index: int) -> str:
+        """ Transforms a filename based on the user-provided parameters.
+
+        Args:
+            path (str): The absolute path of a file.
+            index (int): The index of the current file being renamed.
+        
+        Returns:
+            str: The transformed filename (not a path)."""
+        raise NotImplementedError
+
+
+class BasicRenamer(Renamer):
+    """ A widget forsimple renaming with controls for prefix, suffix, name, extension,
+    number, and date/time. """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setup_controls()
+        self.setup_layout()
+
+    def setup_layout(self) -> None:
+        """ Sets up the layout for the renamer window. """
+        # Basic controls
+        layout = QGridLayout()
+        layout.addWidget(QLabel('Prefix:'), 0, 0)
+        layout.addWidget(self.ptype, 0, 1)
+        layout.addWidget(self.pvalue, 0, 2)
+        layout.addWidget(QLabel('Suffix:'), 1, 0)
+        layout.addWidget(self.stype, 1, 1)
+        layout.addWidget(self.svalue, 1, 2)
+        layout.addWidget(QLabel('Name:'), 2, 0)
+        layout.addWidget(self.ntype, 2, 1)
+        layout.addWidget(self.nvalue, 2, 2)
+        layout.addWidget(QLabel('Extension:'), 3, 0)
+        layout.addWidget(self.etype, 3, 1)
+        layout.addWidget(self.evalue, 3, 2)
+        # Number
+        group = QGroupBox('Number')
+        grouplayout = QFormLayout()
+        grouplayout.addRow('Start number:', self.start)
+        grouplayout.addRow('Number of digits:', self.digits)
+        group.setLayout(grouplayout)
+        layout.addWidget(group, 4, 0, 1, 3)
+        # Date & Time
+        group = QGroupBox('Date and Time')
+        grouplayout = QFormLayout()
+        grouplayout.addRow('Date:', self.date)
+        group.setLayout(grouplayout)
+        layout.addWidget(group, 5, 0, 1, 3)
+        # set layout
+        self.setLayout(layout)
+
+    def setup_controls(self) -> None:
+        """ Sets up the transformation widgets for the selected files. """
+        # Prefix
+        self.pvalue = QLineEdit(self)
+        self.ptype = QComboBox(self)
+        self.ptype.addItems(['Custom', 'Number', 'Date'])
+        self.ptype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.pvalue))
+        # Suffix
+        self.svalue = QLineEdit(self)
+        self.stype = QComboBox(self)
+        self.stype.addItems(['Custom', 'Number', 'Date'])
+        self.stype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.svalue))
+        # Name
+        self.nvalue = QLineEdit(self)
+        self.nvalue.setDisabled(True)
+        self.ntype = QComboBox(self)
+        self.ntype.addItems(['Source name', 'lower case', 'UPPER CASE', 'Title Case',
+                             'Capitalize text', 'Custom name'])
+        self.ntype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.nvalue))
+        # Extension
+        self.evalue = QLineEdit(self)
+        self.evalue.setDisabled(True)
+        self.etype = QComboBox(self)
+        self.etype.addItems(['Source extension', 'lower case', 'UPPER CASE', 'Title Case',
+                             'Capitalize text', 'Custom extension'])
+        self.etype.currentTextChanged.connect(lambda text: self.deactivate_field(text, self.evalue))
+        # Number
+        self.digits = QSpinBox(self)
+        self.start = QSpinBox(self)
+        # Date
+        self.date = QDateEdit(self)
+        self.date.setDisplayFormat('dd-MM-yyyy')
+        self.date.setCalendarPopup(True)
+
+    def transform(self, path: str, index: int) -> str:
+        """ Performs the string transformation based on the selected options. """
+        result = ''
+        match self.ptype.currentText():
+            case 'Number':
+                result += str(self.start.value() + index).zfill(self.digits.value())
+            case 'Date':
+                result += self.date.date().toString('dd-MM-yyyy')
+            case 'Custom':
+                result += self.pvalue.text()
+        basename = os.path.splitext(os.path.basename(path))[0]
+        match self.ntype.currentText():
+            case 'Source name':
+                result += basename
+            case 'lower case':
+                result += basename.lower()
+            case 'UPPER CASE':
+                result += basename.upper()
+            case 'Title Case':
+                result += basename.title()
+            case 'Capitalize text':
+                result += basename.capitalize()
+            case 'Custom name':
+                result += self.nvalue.text()
+        match self.stype.currentText():
+            case 'Number':
+                result += str(self.start.value() + index).zfill(self.digits.value())
+            case 'Date':
+                result += self.date.date().toString('dd-MM-yyyy')
+            case 'Custom':
+                result += self.svalue.text()
+        extension = os.path.splitext(os.path.basename(path))[1]
+        match self.etype.currentText():
+            case 'Source extension':
+                result += extension
+            case 'lower case':
+                result += extension.lower()
+            case 'UPPER CASE':
+                result += extension.upper()
+            case 'Title Case':
+                result += extension.title()
+            case 'Capitalize':
+                result += extension.capitalize()
+            case 'Custom extension':
+                if self.evalue.text():
+                    result += '.'
+                    result += self.evalue.text()
+        return result
+
+    def deactivate_field(self, text: str, field: QLineEdit) -> None:
+        """ Deactivates the transformation fields when specific option is chosen """
+        if text.startswith('Custom'):
+            field.setDisabled(False)
+        else:
+            field.setDisabled(True)
+
+
+class AdvancedRenamer(Renamer):
+    """ A widget for advanced renaming based on provided regular expressions. """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setup_controls()
+        self.setup_layout()
+
+    def setup_controls(self) -> None:
+        """ Sets up fields for the transformation regular expressions. """
+        self.inreg = QLineEdit(self)
+        self.outreg = QLineEdit(self)
+
+    def setup_layout(self) -> None:
+        """ Sets up the layout for the advanced renamer window. """
+        layout = QFormLayout()
+        layout.addRow('Input regular expression:', self.inreg)
+        layout.addRow('Output regular expression:', self.outreg)
+        self.setLayout(layout)
+
+    def transform(self, path: str, index: int) -> str:
+        """ Performs the string transformation based on the provided regular expressions. """
+        # TODO: Implement
+        #return re.sub(self.inreg.text(), self.outreg.text(), path)
