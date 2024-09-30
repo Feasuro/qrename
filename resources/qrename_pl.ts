a9f7fd91d378ba5d56cbff03c0c716a299a1a9b4
Feasuro <feasuro@protonmail.com>
2024-09-30 23:19:21 +0200

    localization support - polish translation

---
+++ resources/qrename_pl.ts
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>qrename</name>
+    <message>
+        <location filename="../qrename.py" line="32"/>
+        <source>Basic Renamer</source>
+        <translation>Proste Przemianowanie</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="33"/>
+        <source>Advanced Renamer</source>
+        <translation>Zaawansowane Przemianowanie</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="38"/>
+        <source>Open Files</source>
+        <translation>Otwórz Pliki</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="40"/>
+        <source>Add Files</source>
+        <translation>Dodaj Pliki</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="48"/>
+        <source>Selected Files</source>
+        <translation>Wybrane Pliki</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="54"/>
+        <source>Rename Files</source>
+        <translation>Zmień Nazwy</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="61"/>
+        <source>New Names</source>
+        <translation>Nowe Nazwy</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="93"/>
+        <source>Select Files</source>
+        <translation>Wybierz Pliki</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="93"/>
+        <source>All Files (*)</source>
+        <translation>Wszystkie Pliki (*)</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="125"/>
+        <source>File exists</source>
+        <translation>Plik już istnieje</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="126"/>
+        <source>File {} already exists, do you want to overwrite it?</source>
+        <translation>Plik {} już istnieje, czy chcesz go nadpisać?</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="134"/>
+        <location filename="../qrename.py" line="137"/>
+        <location filename="../qrename.py" line="140"/>
+        <location filename="../qrename.py" line="143"/>
+        <location filename="../qrename.py" line="146"/>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="135"/>
+        <source>FileNotFoundError: The file &apos;{}&apos; was not found.</source>
+        <translation>FileNotFoundError: Plik &apos;{}&apos; nie został odnaleziony.</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="138"/>
+        <source>PermissionError: Permission denied when renaming the file &apos;{}&apos;.</source>
+        <translation>PermissionError: Brak uprawnień do zmiany nazwy pliku &apos;{}&apos;.</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="141"/>
+        <source>IsADirectoryError: &apos;{}&apos; is a directory, not a file.</source>
+        <translation>IsADirectoryError: &apos;{}&apos; jest katalogiem, nie plikiem.</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="144"/>
+        <source>OSError: An OS error occurred when renaming &apos;{}&apos;: {}</source>
+        <translation>OSError: Wystąpił błąd systemowy podczas zmiany nazwy &apos;{}&apos;: {}</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="147"/>
+        <source>Unexpected error when renaming &apos;{}&apos;: {}</source>
+        <translation>Nieoczekiwany błąd podczas zmiany nazwy &apos;{}&apos;: {}</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="160"/>
+        <source>Rename files based on user-provided parameters.</source>
+        <translation>Zmienia nazwy plików na podstawie parametrów podanych przez użytkownika.</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="163"/>
+        <source>file</source>
+        <translation>plik</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="164"/>
+        <source>files to be renamed or containing directory path</source>
+        <translation>pliki, których nazwy mają zostać zmienione lub ścieżka do zawierającego je katalogu</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="165"/>
+        <source>[directory/file1 file2 ...]</source>
+        <translation>[katalog/plik1 plik2 ...]</translation>
+    </message>
+    <message>
+        <location filename="../qrename.py" line="181"/>
+        <source>Error: File &apos;{}&apos; does not exist.
+</source>
+        <translation>Błąd: Plik &apos;{}&apos; nie istnieje.
+</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="44"/>
+        <source>Prefix:</source>
+        <translation>Przedrostek:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="47"/>
+        <source>Suffix:</source>
+        <translation>Przyrostek:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="50"/>
+        <source>Name:</source>
+        <translation>Nazwa:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="53"/>
+        <source>Extension:</source>
+        <translation>Rozszerzenie:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="57"/>
+        <source>Number</source>
+        <translation>Numer</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="59"/>
+        <source>Start number:</source>
+        <translation>Numer początkowy:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="60"/>
+        <source>Number of digits:</source>
+        <translation>Liczba cyfr:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="64"/>
+        <source>Date and Time</source>
+        <translation>Data i Godzina</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="66"/>
+        <source>Date:</source>
+        <translation>Data:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="67"/>
+        <source>Date format:</source>
+        <translation>Format daty:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="78"/>
+        <source>Custom</source>
+        <translation>Własny</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="78"/>
+        <source>Date</source>
+        <translation>Data</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="89"/>
+        <source>Custom name</source>
+        <translation>Własna nazwa</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="89"/>
+        <source>Source name</source>
+        <translation>Nazwa źródłowa</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="89"/>
+        <source>lower case</source>
+        <translation>małe litery</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="90"/>
+        <source>UPPER CASE</source>
+        <translation>WIELKIE LITERY</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="90"/>
+        <source>Title Case</source>
+        <translation>Pierwsza W Wyrazie</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="90"/>
+        <source>Capitalize text</source>
+        <translation>Pierwsza litera</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="97"/>
+        <source>Custom extension</source>
+        <translation>Własne rozszerzenie</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="97"/>
+        <source>Source extension</source>
+        <translation>Rozszerzenie źródłowe</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="186"/>
+        <source>Pattern regular expression:</source>
+        <translation>Wzorzec wyrażenia regularnego:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="187"/>
+        <source>Replacement:</source>
+        <translation>Zamiennik:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="188"/>
+        <source>Maximal number of replacements:</source>
+        <translation>Maksymalna liczba zastąpień:</translation>
+    </message>
+    <message>
+        <location filename="../renamers.py" line="197"/>
+        <source>Warning: Invalid regular expression: {}</source>
+        <translation>Ostrzeżenie: nieprawidłowe wyrażenie regularne: {}</translation>
+    </message>
+</context>
+</TS>
